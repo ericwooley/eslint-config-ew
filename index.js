a223cc2d3f59f8b23e31526e9e635aabf1d62a91
Eric Wooley <Eric.Wooley@rosetta.com>
2015-12-07 15:19:26 -0800

    got it working

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 var fs = require('fs')
 var rules = JSON.parse(fs.readFileSync('./.eslintrc'))
-rules.rules = {
-  // "camel_case": require('./camel_case'),
-  // "no-commented-out-code": require('./no-commented-out-code'),
-  // "no-for-loops": require('./no-for-loops'),
-  'no-long-files': require('eslint-rules/no-long-files')
-  // "no-single-line-objects": require('./no-single-line-objects'),
-  // "potential-point-free": require('./potential-point-free')
-}
 module.exports = rules
